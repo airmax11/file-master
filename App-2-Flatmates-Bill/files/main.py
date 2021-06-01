47ef75ac28953bd06d70976ef471695738dfc3f7
airmax11 <maksvellspam@gmail.com>
2021-06-01 18:13:06 +0200

    new funct. in App2

--- App-2-Flatmates-Bill/files/main.py
+++ App-2-Flatmates-Bill/files/main.py
@@ -1,3 +1,7 @@
+import webbrowser
+
+from fpdf import FPDF
+
 class Bill:
     """
     Object that contains data about a bill, such as amount.
@@ -27,7 +31,33 @@ class PdfReport:
         self.filename = filename
 
     def generate_report(self, flatmate_1, flatmate_2, bill):
-        pass
+        flatmate_1_pay = str(round(flatmate_1.pays(bill, flatmate_2), 2))
+        flatmate_2_pay = str(round(flatmate_2.pays(bill, flatmate_1), 2))
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+
+        pdf.image("house.png", w=30, h=30)
+
+        #Add some text
+        pdf.set_font(family='Times', size=24, style='B')
+        pdf.cell(w=0, h=80, txt='Flatmates Bill', border=1, align='C', ln=1)
+
+        pdf.set_font(family='Times', size=15, style='B')
+        pdf.cell(w=100, h=40, txt='Period:', border=0)
+        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
+
+        pdf.set_font(family='Times', size=12)
+        pdf.set_text_color(120, 30, 200)
+        #Add some text to the report
+        pdf.cell(w=100, h=25, txt=flatmate_1.name, border=0)
+        pdf.cell(w=150, h=25, txt=flatmate_1_pay, border=0, ln=1)
+        #Add some text to the report
+        pdf.cell(w=100, h=25, txt=flatmate_2.name, border=0)
+        pdf.cell(w=150, h=25, txt=flatmate_2_pay, border=0, ln=1)
+
+        pdf.output(f"{self.filename}.pdf")
+
+        webbrowser.open(f"{self.filename}.pdf")
 
 
 the_bill = Bill(120, "March 2021")
@@ -36,3 +66,6 @@ mary = Flatmates("Mary", 25)
 
 print("john pays: ", john.pays(the_bill, mary))
 print("mary pays: ", mary.pays(the_bill, john))
+
+pdfreport = PdfReport("bill")
+pdfreport.generate_report(john, mary, the_bill)

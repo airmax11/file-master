b8491e4b9c59da61cfaabff44d09e3c33437cb8f
airmax11 <maksvellspam@gmail.com>
2021-06-02 15:43:25 +0200

    Refactor

---
+++ App-2-Flatmates-Bill/main.py
@@ -0,0 +1,17 @@
+from flat import Bill, Flatmates
+from reports import PdfReport
+
+amount = float(input("Enter bill amount: "))
+period = input("Enter time period: ")
+flatmate_1_name_days = input("Enter Flatmate Name and days of stay, divided by ',': ").split(",")
+flatmate_2_name_days = input("Enter Flatmate Name and days of stay, divided by ',': ").split(",")
+
+the_bill = Bill(amount, period)
+flatmate_1 = Flatmates(flatmate_1_name_days[0], int(flatmate_1_name_days[1]))
+flatmate_2 = Flatmates(flatmate_2_name_days[0], int(flatmate_2_name_days[1]))
+
+print(f"{flatmate_1_name_days[0]} pays: ", flatmate_1.pays(the_bill, flatmate_2))
+print(f"{flatmate_2_name_days[0]} pays: ", flatmate_2.pays(the_bill, flatmate_1))
+
+pdfReport = PdfReport("bill")
+pdfReport.generate_report(flatmate_1, flatmate_2, the_bill)
--- App-2-Flatmates-Bill/reports.py
+++ App-2-Flatmates-Bill/reports.py
@@ -1,27 +1,5 @@
-import webbrowser
-
 from fpdf import FPDF
-
-class Bill:
-    """
-    Object that contains data about a bill, such as amount.
-    """
-    def __init__(self, amount, period):
-        self.period = period
-        self.amount = amount
-
-
-class Flatmates:
-    """ Creates a flatmates who lives in the flat and pays a share off the bill."""
-    
-    def __init__(self, name, days_in_house):
-        self.days_in_house = days_in_house
-        self.name = name
-
-    def pays(self, bill, cooflatmate):
-        weight = self.days_in_house / (self.days_in_house + cooflatmate.days_in_house)
-        to_pay = bill.amount * weight
-        return to_pay
+import os
 
 
 class PdfReport:
@@ -36,9 +14,9 @@ class PdfReport:
         pdf = FPDF(orientation='P', unit='pt', format='A4')
         pdf.add_page()
 
-        pdf.image("house.png", w=30, h=30)
+        pdf.image("files/house.png", w=30, h=30)
 
-        #Add some text
+        # Add some text
         pdf.set_font(family='Times', size=24, style='B')
         pdf.cell(w=0, h=80, txt='Flatmates Bill', border=1, align='C', ln=1)
 
@@ -48,24 +26,15 @@ class PdfReport:
 
         pdf.set_font(family='Times', size=12)
         pdf.set_text_color(120, 30, 200)
-        #Add some text to the report
+        # Add some text to the report
         pdf.cell(w=100, h=25, txt=flatmate_1.name, border=0)
         pdf.cell(w=150, h=25, txt=flatmate_1_pay, border=0, ln=1)
-        #Add some text to the report
+        # Add some text to the report
         pdf.cell(w=100, h=25, txt=flatmate_2.name, border=0)
         pdf.cell(w=150, h=25, txt=flatmate_2_pay, border=0, ln=1)
 
+        # Change directory to files and generate&open report
+        os.chdir("files")
         pdf.output(f"{self.filename}.pdf")
 
-        webbrowser.open(f"{self.filename}.pdf")
-
-
-the_bill = Bill(120, "March 2021")
-john = Flatmates("John", 20)
-mary = Flatmates("Mary", 25)
-
-print("john pays: ", john.pays(the_bill, mary))
-print("mary pays: ", mary.pays(the_bill, john))
-
-pdfreport = PdfReport("bill")
-pdfreport.generate_report(john, mary, the_bill)
+        # webbrowser.open(f"{self.filename}.pdf")

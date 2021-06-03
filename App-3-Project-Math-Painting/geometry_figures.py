e921816ac4e20e26ea19867158fd6f6c628ef56a
airmax11 <maksvellspam@gmail.com>
2021-06-03 21:30:45 +0200

    Geometry figures app image

---
+++ App-3-Project-Math-Painting/geometry_figures.py
@@ -0,0 +1,16 @@
+class Square:
+
+    def __init__(self):
+        pass
+
+    def draw_square(self):
+        pass
+
+
+class Rectangle:
+
+    def __init__(self):
+        pass
+
+    def draw_rectangle(self):
+        pass
---
+++ App-3-Project-Math-Painting/image_processing.py
@@ -0,0 +1,7 @@
+class Canvas:
+
+    def __init__(self):
+        pass
+
+    def produce_imagee(self):
+        pass

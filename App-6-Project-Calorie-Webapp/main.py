8db8b39338d9a1ac6d5ee34ffcfa8bb042c2d7a7
airmax11 <maksvellspam@gmail.com>
2021-06-11 15:56:55 +0200

    app6

--- App-6-Project-Calorie-Webapp/main.py
+++ App-6-Project-Calorie-Webapp/main.py
@@ -12,12 +12,44 @@ class HomePage(MethodView):
         return render_template("index.html")
 
 
-class CaloriesCalcPage(MethodView):
+class CaloriesCalcFormPage(MethodView):
     def get(self):
-        return render_template("calc.html")
+        calc_form = CaloriesCalcForm()
+        return render_template("calc.html", calcform=calc_form)
+
+
+class ResultsPage(MethodView):
+
+    def post(self):
+        calcform = CaloriesCalcForm(request.form)
+        weight = float(calcform.weight.data)
+        height = float(calcform.height.data)
+
+        # the_bill = Bill(amount, period)
+        #
+        # name_1 = calcform.name1.data
+        # name_1_dates_in_house = float(calcform.days_in_house_1.data)
+        # flatmate_1 = Flatmates(name_1, name_1_dates_in_house)
+        #
+        # name_2 = calcform.name2.data
+        # name_2_dates_in_house = float(calcform.days_in_house_2.data)
+        # flatmate_2 = Flatmates(name_2, name_2_dates_in_house)
+        #
+        # return render_template("results.html", name1=flatmate_1.name, amount1=flatmate_1.pays(the_bill, flatmate_2),
+        #                        name2=flatmate_2.name, amount2=flatmate_2.pays(the_bill, flatmate_1))
+
+
+class CaloriesCalcForm(Form):
+    weight = StringField("Weight:", default=80)
+    height = StringField("Height (sm):", default=170)
+    age = StringField("Age:", default=30)
+    city = StringField("City:", default='New-York')
+    age = StringField("Country:", default='USA')
+
+    button = SubmitField("Calculate")
 
 
 app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
-app.add_url_rule('/calorie_calc', view_func=CaloriesCalcPage.as_view('calc_page'))
+app.add_url_rule('/calorie_calc', view_func=CaloriesCalcFormPage.as_view('calc_page'))
 
 app.run(debug=True)

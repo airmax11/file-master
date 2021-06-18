ec6b98d9361f3a1682cb531511f6e8b5d0ddfbac
airmax11 <maksvellspam@gmail.com>
2021-06-18 20:44:25 +0200

    News api

--- App-7-Automated-Emails/news_feed.py
+++ App-7-Automated-Emails/news_feed.py
@@ -1,8 +1,30 @@
+import requests
+import pprint
+
 # API KEY=f3526a707083479b9cdd627ead38ffed
 class NewsFeed:
 
-    def __init__(self, data):
-        self.data = data
+    URL = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=f3526a707083479b9cdd627ead38ffed"
+
+    # def __init__(self, data):
+    #     self.data = data
 
     def get_info(self):
-        pass
+        response = requests.get(self.URL).json()
+        articles = response['articles']
+
+        for i in articles:
+            if i['author'] == None:
+                print(i['title'])
+                print(i['url'])
+                print()
+            else:
+                print(i['author'])
+                print(i['title'])
+                print(i['url'])
+                print()
+
+
+
+
+test = NewsFeed().get_info()
